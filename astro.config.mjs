a3a7bfe8d496cd71f541360f250a165969f6eb4c
ultimatefergal <fernando.galarcio@gmail.com>
2025-01-21 20:47:23 -0500

    add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,13 @@ import { defineConfig, envField } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   integrations: [tailwind()],
+
   env: {
     schema: {
       SHOW_BUY_BUTTON: envField.boolean({
@@ -20,4 +23,6 @@ export default defineConfig({
       }),
     },
   },
+
+  adapter: vercel(),
 });
